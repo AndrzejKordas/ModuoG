af713399595a122dc27cb6e077306f31ec2c110a
Andrzej4 <ahills@o2.pl>
2018-09-28 15:53:12 +0200

    Poczatek

---
+++ js/ele.js
@@ -0,0 +1,6 @@
+const butClose = document.getElementById("close");
+const Black = document.getElementById("komunikat");
+function zamknij() {
+    Black.style.display = "none";
+}
+butClose.addEventListener("zamknij", close, false);
